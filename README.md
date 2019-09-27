d2e4c61fa361ea92dcd19add76be721e9f75befb
uchida_kei <chanomijikan@gmail.com>
2019-09-27 15:59:17 +0900

    add script

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # tree2json
+
+ref:https://gist.github.com/shimizu/7bd8c02c3d49492d1350
+
+The code of the above reference is changed to typescript
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,42 @@
-function hello(name: string): string {
-	return `Hello, ${name}!!!!!`;
+//ref:https://gist.github.com/shimizu/7bd8c02c3d49492d1350
+
+import fs from "fs";
+import path from "path";
+const dir = process.argv[2] || '.';
+
+const walk = (dir_path: string, callback: (err: NodeJS.ErrnoException | null, res: {}[]) => void) => {
+	const results: {}[] = [];
+	fs.readdir(dir_path, (err, files) => {
+		if (err) {
+			throw err;
+		}
+		let pending = files.length;
+		if (!pending) {
+			return callback(null, results);
+		}
+		files.map(file => path.join(dir_path, file)).filter(file => {
+			if(fs.statSync(file).isDirectory()) {
+				walk(file, (err, res) => {
+					results.push({name:path.basename(file), children:res});
+					if (!--pending) {
+						callback(null, results);
+					}
+				});
+			}
+			return fs.statSync(file).isFile();
+		}).forEach(file => {
+			results.push({file:path.basename(file)});
+			if (!--pending) {
+				callback(null, results);
+			}
+		});
+	});
 }
 
-console.log(hello("typeScript"));
+walk(dir, (err, results) => {
+	if (err) {
+		throw err;
+	}
+	const data = {name:'root', children:results};
+	console.log(JSON.stringify(data));
+});
